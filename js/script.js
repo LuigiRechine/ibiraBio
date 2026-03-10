1d1bb7e1d1e6dbef08f9bdb8946ef84480e2bf10
LuigiRechine <lbarrosrechinelli@gmail.com>
2026-03-10 20:05:18 -0300

    alteracao

--- js/script.js
+++ js/script.js
@@ -31,31 +31,3 @@ async function loadSpecies(){
 
 loadSpecies()
 
-const lista = document.getElementById('speciesContainer');
-const input = document.getElementById('search');
-
-let especies = [];
-
-// Pega tudo do backend uma vez
-fetch('https://projeto-biodiversidade-production.up.railway.app/species')
-  .then(res => res.json())
-  .then(data => {
-    especies = data;
-    mostrarLista(especies);
-  });
-
-function mostrarLista(dados) {
-  lista.innerHTML = '';
-  dados.forEach(item => {
-    const li = document.createElement('li');
-    li.textContent = item.name; // ou outro campo que quiser mostrar
-    lista.appendChild(li);
-  });
-}
-
-// Filtra localmente enquanto digita
-input.addEventListener('input', () => {
-  const termo = input.value.toLowerCase();
-  const filtradas = especies.filter(e => e.name.toLowerCase().includes(termo));
-  mostrarLista(filtradas);
-});
