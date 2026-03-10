15f9673113f405d01620374561d701f5f03b5fda
LuigiRechine <lbarrosrechinelli@gmail.com>
2026-03-10 20:07:40 -0300

    usandoSearch

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
 const container = document.getElementById("speciesContainer")
+const searchInput = document.getElementById("search")
 
-async function loadSpecies(){
+let speciesList = [] // global, pra manter a lista original
 
+async function loadSpecies() {
     const response = await fetch("https://projeto-biodiversidade-production.up.railway.app/species")
-    const speciesList = await response.json()
+    speciesList = await response.json() // salva globalmente
 
-    container.innerHTML = ""
+    mostrarSpecies(speciesList)
+}
 
-    speciesList.forEach(species => {
+function mostrarSpecies(list) {
+    container.innerHTML = "" // limpa container
 
+    list.forEach(species => {
         const card = document.createElement("div")
         card.className = "card"
-
         card.innerHTML = `
             <div class="card-img-container">
                 <img src="${species.imageUrl}" class="card-image" alt="foto da espécie">
             </div>
-
             <h3>${species.nome}</h3>
             <p><i>${species.nomeCientifico}</i></p>
             <p>Tipo: ${species.tipo}</p>
             <p>Porte: ${species.porte}</p>
         `
-
         container.appendChild(card)
-
     })
-
 }
+
+// filtra enquanto digita
+searchInput.addEventListener("input", () => {
+    const termo = searchInput.value.toLowerCase()
+    const filtradas = speciesList.filter(s => 
+        s.nome.toLowerCase().includes(termo) || 
+        s.nomeCientifico.toLowerCase().includes(termo) ||
+        s.tipo.toLowerCase().includes(termo)
+    )
+    mostrarSpecies(filtradas)
+})
 
 loadSpecies()
 
